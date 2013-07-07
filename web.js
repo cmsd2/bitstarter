d6ed9f55bafb7cdf12a823975e6b92b7fcd21823
Chris Dawes <cmsdawes@googlemail.com>
2013-07-07 20:59:06 +0200

    serve index.html

--- web.js
+++ web.js
@@ -1,9 +1,27 @@
-var express = require('express');
+var express = require('express'),
+fs = require('fs'),
+path = require('path'),
+util = require('util');
 
 var app = express.createServer(express.logger());
 
+var streamFile = function(name, mimeType, response) {
+  var filePath = path.join(__dirname, name);
+
+  var stat = fs.statSync(filePath);
+
+  response.writeHead(200, {
+    'Content-Type': mimeType,
+    'Content-Length': stat.size
+  });
+
+  var stream = fs.createReadStream(filePath, {encoding: 'utf-8'});
+
+  util.pump(stream, response);
+};
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  streamFile('index.html', 'text/plain', response);
 });
 
 var port = process.env.PORT || 5000;
